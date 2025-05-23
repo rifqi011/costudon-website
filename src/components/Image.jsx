e62ddc52ee3f623475aaa3d5700a2ff328b8448f
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-05-23 19:24:36 +0700

    fix: Restore image styling in Image component for consistency

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({ src, alt = "Image", className }) {
 	return (
-		<div className={`object-cover overflow-hidden shadow-black rounded-xl border-3 border-black ${className}`}>
-			<img src={src} alt={alt} />
+		<div className={`overflow-hidden shadow-black rounded-xl border-3 border-black ${className}`}>
+			<img src={src} alt={alt} className="object-cover bg-cover h-full w-full" />
 		</div>
 	)
 }
