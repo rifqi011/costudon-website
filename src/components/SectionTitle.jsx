2c49a79ec0dbb511d81d5f0d4f691cd109553c84
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-05-24 21:19:31 +0700

    feat: Update SectionTitle component for improved styling and alignment options

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,8 +1,8 @@
 export default function SectionTitle({ align, title, description, className }) {
 	return (
 		<div className={`flex flex-col gap-6 ${align === "center" ? "items-center" : "items-start"} ${className}`}>
-            <h2 className="text-2xl font-bold">{title}</h2>
-            <p className="text-base">{description}</p>
+            <h2 className="text-3xl font-bold">{title}</h2>
+            <p className={`text-base ${align === "center" ? "text-center" : "text-left"}`}>{description}</p>
 		</div>
 	)
 }
