1006752b0936df6ed2a2655a479d00fa215b6149
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-06-04 19:00:14 +0700

    feat: Add onClick prop to Image component for enhanced interactivity

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export default function Image({ src, alt = "Image", className }) {
+export default function Image({ src, alt = "Image", className, onClick }) {
 	const [isClicked, setIsClicked] = useState(false)
 
 	const handleClick = () => {
