a4e6a8f2c354c09d6c4711ae33285e0855c09946
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-06-04 18:54:16 +0700

    feat: Implement click animation for Image component and refine CSS keyframes

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,6 +1,23 @@
+import { useState } from "react"
+
 export default function Image({ src, alt = "Image", className }) {
+	const [isClicked, setIsClicked] = useState(false)
+
+	const handleClick = () => {
+		setIsClicked(true)
+		setTimeout(() => {
+			setIsClicked(false)
+		}, 200)
+	}
+
 	return (
-		<div className={`overflow-hidden shadow-black rounded-xl border-3 border-black ${className}`}>
+		<div
+			onClick={(e) => {
+				handleClick()
+				onClick?.(e)
+			}}
+			className={`overflow-hidden shadow-black rounded-xl border-3 border-black ${isClicked ? "animate-clicked" : ""} ${className}`}
+		>
 			<img src={src} alt={alt} className="object-cover bg-cover h-full w-full" />
 		</div>
 	)
