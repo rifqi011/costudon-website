64e0e5801346411e050b8178af12aa9094811bc7
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-06-08 18:54:36 +0700

    fix: correct section ID in Members component for consistency

--- src/sections/Members.jsx
+++ src/sections/Members.jsx
@@ -4,7 +4,7 @@ import SectionTitle from "../components/SectionTitle";
 
 export default function Members() {
     return (
-		<Section id={"member"} className={"bg-light-yellow"}>
+		<Section id={"members"} className={"bg-light-yellow"}>
             <SectionTitle title={"Anggota"} description={"Kenali talenta Costudon! Inilah kumpulan individu unik, masing-masing dengan skill dan passion yang siap di-deploy. Mereka adalah building block utama dari union keren ini."} align={"center"} />
             
             <MemberCard />
