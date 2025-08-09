69f4ee3eca8a2447bec5f2ba4386ac42b135c029
Rifqi Banu Safingi <rifqibanusafingi11@gmail.com>
2025-08-09 23:58:50 +0700

    refactor: change achievement images

--- src/sections/Achievements.jsx
+++ src/sections/Achievements.jsx
@@ -4,44 +4,7 @@ import SectionTitle from "../components/SectionTitle"
 import Image from "../components/Image"
 import { useState } from "react"
 import Accordion from "../components/Accordion"
-
-const AchievementsData = [
-	{
-		id: 1,
-		title: "Juara 2 ESport",
-		description: "Pemenang Juara 2 di Esport SMK Negeri 1 Purwokerto pada Classmeet Juni 2025.",
-	},
-	{
-		id: 2,
-		title: "Juara 3 Sambung Melodi",
-		description: "Pemenang Juara 3 di Sambung Melodi SMK Negeri 1 Purwokerto pada Classmeet Juni 2025.",
-	},
-	{
-		id: 3,
-		title: "Juara 1 ESport",
-		description: "Pemenang Juara 1 di Esport SMK Negeri 1 Purwokerto pada Classmeet Desember 2024.",
-	},
-	{
-		id: 4,
-		title: "Juara 1 Kempit Sedotan",
-		description: "Pemenang Juara 1 di Kempit Sedotan SMK Negeri 1 Purwokerto pada Classmeet Desember 2024.",
-	},
-	{
-		id: 5,
-		title: "Juara 3 Futsal Putra",
-		description: "Pemenang Juara 3 di Futsal Putra SMK Negeri 1 Purwokerto pada Classmeet Desember 2024.",
-	},
-	{
-		id: 6,
-		title: "Juara 2 ESport",
-		description: "Pemenang Juara 2 di Esport SMK Negeri 1 Purwokerto pada Classmeet Agustus 2024.",
-	},
-	{
-		id: 7,
-		title: "Juara 2 Vlog Job Fair",
-		description: "Pemenang Juara 2 Vlog Job Fair SMK Negeri 1 Purwokerto tahun 2024",
-	},
-]
+import AchievementsData from "../data/achievements.json"
 
 export default function Achievements() {
 	const [activeIndex, setActiveIndex] = useState(0)
@@ -57,7 +20,7 @@ export default function Achievements() {
 			<SectionTitle title={"Milestone Tracker"} description={"Lihatlah jejak gemilang yang telah kami ukir! Setiap pencapaian adalah milestone yang menginspirasi kami untuk terus berinovasi dan meraih lebih banyak lagi."} />
 
 			<div className="flex flex-col gap-6 md:flex-row">
-				<Image src={`./images/achievements/${activeIndex + 1}.jpg`} className={`aspect-[4/3] w-full`} alt="Foto Prestasi" />
+				<Image src={`./${AchievementsData[activeIndex].image}`} className={`aspect-[4/3] w-full`} alt="Foto Prestasi" />
 
 				<div className="flex flex-col gap-4">
 					{AchievementsData.map((data, index) => (
